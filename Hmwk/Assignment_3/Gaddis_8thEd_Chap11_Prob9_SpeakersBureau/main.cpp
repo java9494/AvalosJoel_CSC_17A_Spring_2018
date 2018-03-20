22cbb050998370b83951e68dd3152f12640a4ad9
java9494 <joelavalosjr@gmail.com>
2018-03-20 11:39:11 -0700

    Signed-off-by: java9494 <joelavalosjr@gmail.com>

---
+++ Hmwk/Assignment_3/Gaddis_8thEd_Chap11_Prob9_SpeakersBureau/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Joel Avalos
+ * Created on March 20, 2018, 10:56 AM
+ * Purpose: Create a program that keeps track of a speakers' bureau.
+ */
+ 
+ //System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+            
+    //output data
+            
+    //Exit stage right!
+    return 0;
+}
