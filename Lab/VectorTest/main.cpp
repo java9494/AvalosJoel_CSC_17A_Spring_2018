dc51eb364944273e51061cedea23266e72810072
java9494 <joelavalosjr@gmail.com>
2018-05-16 14:50:46 -0700

    Signed-off-by: java9494 <joelavalosjr@gmail.com>

---
+++ Lab/VectorTest/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Joel Avalos
+ * Purpose: Test out SimpleVector, and create a push_back and pop_back functions
+ * Created on May 16, 2018, 2:36 PM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+#include "SimpleVector.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int SIZE=10;
+    SimpleVector<int> vector(SIZE);
+    return 0;
+}
+
